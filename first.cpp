2567dc95fb066a7bbaca117b1624034cbb700e04
John Doe <xzzz9097@gmail.com>
2014-09-23 20:03:43 +0200

    Correctly indent Copy/paste transforms tabs in spaces.

--- first.cpp
+++ first.cpp
@@ -8,26 +8,26 @@ using namespace std;
 
 int main()
 {
-	// Initialize vars
-	int a = 0, b = 0;
+    // Initialize vars
+    int a = 0, b = 0;
 
-	// Get the inputs
-	cout << "Write first number: ";
-	cin >> a;
-	cout << "Write second number: ";
-  cin >> b;
-  bool isSecondNumberPositive = b > 0;
+    // Get the inputs
+    cout << "Write first number: ";
+    cin >> a;
+    cout << "Write second number: ";
+    cin >> b;
+    bool isSecondNumberPositive = b > 0;
 
-  // Do the math
-  int sum = a + b;
+    // Do the math
+    int sum = a + b;
 
-  // Just a bit of polish for the negative 2nd number case
-	string sign = isSecondNumberPositive ? " + " : " - ";
-  b = isSecondNumberPositive ? b : -b;
-  // Output the operation
-  // We append a intro, the operation and the newline char
-  cout << a << sign << b << " = " << sum << "\n";
+    // Just a bit of polish for the negative 2nd number case
+    string sign = isSecondNumberPositive ? " + " : " - ";
+    b = isSecondNumberPositive ? b : -b;
+    // Output the operation
+    // We append a intro, the operation and the newline char
+    cout << a << sign << b << " = " << sum << "\n";
 
-  // Return statement
-  return 0;
+    // Return statement
+    return 0;
 }
