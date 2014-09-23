91a4f5ec48beceb9ad7b6818caac914e1e2f1023
John Doe <xzzz9097@gmail.com>
2014-09-23 16:22:21 +0200

    Some adjustements

--- first.cpp
+++ first.cpp
@@ -12,14 +12,21 @@ int main()
 	int a = 0, b = 0;
 
 	// Get the inputs
+	cout << "Write first number: ";
 	cin >> a;
+	cout << "Write second number: ";
   cin >> b;
+  bool isSecondNumberPositive = b > 0;
 
   // Do the math
   int sum = a + b;
 
-  // Output the result
-  cout << sum;
+  // Just a bit of polish for the negative 2nd number case
+	string sign = isSecondNumberPositive ? " + " : " - ";
+  b = isSecondNumberPositive ? b : -b;
+  // Output the operation
+  // We append a intro, the operation and the newline char
+  cout << a << sign << b << " = " << sum << "\n";
 
   // Return code
   return 0;
