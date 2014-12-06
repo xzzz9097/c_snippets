e12e6f00615bd6b7ef2ca292bb524a53dc856bac
John Doe <xzzz9097@gmail.com>
2014-12-06 22:29:17 +0100

    Naming change

--- puntiSignificativiParabola.cpp
+++ puntiSignificativiParabola.cpp
@@ -15,8 +15,8 @@ char TITOLO_INTERSEZIONI[] = "Intersezioni";
 char NO_INTERSEZIONI[] = "La parabola non ha intersezioni con l'asse X.";
 
 // Costanti
-const int LUNGHEZZA_ARRAY_INTERSEZIONI = 4;
-const int LUNGHEZZA_ARRAY_PUNTO = 2;
+const int DIM_ARR_INTERSEZIONI = 4;
+const int DIM_ARR_PUNTO = 2;
 
 float calcolaDelta(float a, float b, float c) {
 	return (pow(b, 2.0) - 4 * a * c);
@@ -33,7 +33,7 @@ float togliSegnoSeZero(float numero) {
 
 float * creaArrayIntersezioni(float a, float b, float c, float delta) {
 	// Uso l'attributo static per impedire l'alterazione dell'array al di fuori della funzione
-	static float arraySoluzioni[LUNGHEZZA_ARRAY_INTERSEZIONI];
+	static float arraySoluzioni[DIM_ARR_INTERSEZIONI];
 
 	// Assegno le celle dell'array
 	arraySoluzioni[0] = (- b - sqrt(delta)) / (2 * a);
@@ -43,7 +43,7 @@ float * creaArrayIntersezioni(float a, float b, float c, float delta) {
 }
 
 float * creaArrayFuoco(float a, float b, float c, float delta) {	
-	static float arrayFuoco[LUNGHEZZA_ARRAY_PUNTO];
+	static float arrayFuoco[DIM_ARR_PUNTO];
 
 	arrayFuoco[0] = togliSegnoSeZero((- b) / (2 * a));
 	arrayFuoco[1] = togliSegnoSeZero((1 - delta) / (4 * a));
@@ -52,7 +52,7 @@ float * creaArrayFuoco(float a, float b, float c, float delta) {
 }
 
 float * creaArrayVertice(float a, float b, float c, float delta) {
-	static float arrayVertice[LUNGHEZZA_ARRAY_PUNTO];
+	static float arrayVertice[DIM_ARR_PUNTO];
 
 	arrayVertice[0] = togliSegnoSeZero((- b) / (2 * a));
 	arrayVertice[1] = togliSegnoSeZero((- delta) / (4 * a));
@@ -60,7 +60,7 @@ float * creaArrayVertice(float a, float b, float c, float delta) {
 	return arrayVertice;
 }
 
-void leggiArray(float * puntatoreArray, float lunghezzaArray, char titoloOutput[]) {
+void leggiCoordinate(float * puntatoreArray, float lunghezzaArray, char titoloOutput[]) {
 	for (int i = 0; i < lunghezzaArray; i+=2) {
 		// Il puntatore può essere letto come un array, ponendo l'indice fra parentesi quadre
 		cout << titoloOutput << ": (" << puntatoreArray[i] << "; " << puntatoreArray[i+1] << ")" << endl;
@@ -79,12 +79,12 @@ int main() {
 	float mDelta = calcolaDelta(mA, mB, mC);
 
 	// Output fuoco e vertice
-	leggiArray(creaArrayFuoco(mA, mB, mC, mDelta), LUNGHEZZA_ARRAY_PUNTO, TITOLO_FUOCO);
-	leggiArray(creaArrayVertice(mA, mB, mC, mDelta), LUNGHEZZA_ARRAY_PUNTO, TITOLO_VERTICE);
+	leggiCoordinate(creaArrayFuoco(mA, mB, mC, mDelta), DIM_ARR_PUNTO, TITOLO_FUOCO);
+	leggiCoordinate(creaArrayVertice(mA, mB, mC, mDelta), DIM_ARR_PUNTO, TITOLO_VERTICE);
 
 	// Output intersezioni asse X se presenti
 	if(possiedeIntersezioni(mDelta)) {
-		leggiArray(creaArrayIntersezioni(mA, mB, mC, mDelta), LUNGHEZZA_ARRAY_INTERSEZIONI, TITOLO_INTERSEZIONI);
+		leggiCoordinate(creaArrayIntersezioni(mA, mB, mC, mDelta), DIM_ARR_INTERSEZIONI, TITOLO_INTERSEZIONI);
 	} else {
 		cout << NO_INTERSEZIONI << endl; 
 	}
