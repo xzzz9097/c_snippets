2a07a2f9901a2d3573d97f1e28c8e574c81b57a3
John Doe <xzzz9097@gmail.com>
2016-02-29 21:13:50 +0100

    Aggiunta misurazione tempo di esecuzione

--- crivello_eratostene/main.c
+++ crivello_eratostene/main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <time.h>
 #include "crivello.h"
 
 int main() {
+    clock_t inizio = clock();
+
     int *mNumeriPrimi = numeriPrimi(100);
 
     int i = 0;
     while (mNumeriPrimi[i] > 0) {
-        printf("%d\n", mNumeriPrimi[i]);
+        printf("%d ", mNumeriPrimi[i]);
         i++;
     }
+
+    clock_t fine = clock();
+    double trascorso = (double) (fine - inizio) / CLOCKS_PER_SEC;
+
+    printf("%f\n", trascorso);
 }
